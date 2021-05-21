c29222359c946a56081dc181e84b3161cb9156a3
UriGadot <50263385+Ugadot@users.noreply.github.com>
2021-05-21 15:36:37 +0300

    Update CentralBank.sol

--- CentralBank.sol
+++ CentralBank.sol
@@ -1,8 +1,31 @@
 pragma solidity ^0.5.0;
 
 contract CentralBank{
-	address Oracle = 0xaaa;  //TODO: Fill Oracle address here
-  Coin coin_contract; 
+	//address Oracle = 0xaaa;  //TODO: Fill Oracle address here
+	uint mod = 256;
+
+	//Coin to 2 ration if > 1 need to infalte coin base, if < 1 need users to buy bons and lower the amount of total coins
+	float C2D_ratio;
   
-  function 
-}
+	function updateC2D(Coin _coin_contract) public {
+		// C2D_ration = Oracle.getRatio();
+		// currently use random between [0.5, 1.5]
+		uint randomNum = uint(keccak256(abi.encodePacked(
+				  now, 
+				  block.difficulty, 
+				  msg.sender
+				))) % mod;
+		// Make ratio between 0.5 and 1.5
+
+		C2D_ratio = (randomNum + mod / 2) / float(mod / 2);
+		uint current_amount = _coin_contract.getTotalAmount();
+		if (C2D_ratio > 1)
+		{
+			_coin_contract.inflate();
+		}
+	}
+	
+	function getC2D() public returns(float){
+		return C2D_ratio;
+	}
+	
