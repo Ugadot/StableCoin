4219771b9d266e4164fcfd455fabc3e887871e93
UriGadot <50263385+Ugadot@users.noreply.github.com>
2021-05-21 14:56:31 +0300

    Update Coin.sol

--- Coin.sol
+++ Coin.sol
@@ -2,38 +2,36 @@ pragma solidity ^0.5.0;
 
 contract Coin{
 	address CentralBank = 0x70fdAc39369d55464b410e2fCbbd4b78E002eD25;
-	mapping(address => uint) tokens;
-	function approval(address _owner, address _approved,uint _tokenId){
-		require(tokens[_owner]==_tokenId);
-		tokens[_approved]=_tokenId;
-	}
-	function transfer(address _to, uint _amount) public payable{
-		require(_amount <= tokens[msg.sender]);
-		tokens[msg.sender]-=_amount;
-		tokens[_to]+=_amount;
-	}
-	function balanceOf(address _owner) public view returns (uint){
-		return tokens[_owner];
-	}
-	function ownerOf(uint _tokenId) public view returns(address){
-		return tokens[_id].address;
-	}
-	function TransferFrom(address _from, address _to, uint _tokenId) payable{
-		require(tokens[_from]==_tokenId);
-		tokens[_from]=0;
-		tokens[_to]=_tokenId;
-	}
-	function approve(address _approved, uint _tokenId) payable{
-		require(tokens[msg.sender]==_tokenId);
-		tokens[_approved]=_tokenId;
-	}
-	function mint(address _to, uint _amount) payable{
-		tokens[CentralBank]+=_amount;
-		tokens[_to]+=_amount;
+	mapping(address => float) balances;
+	mapping(address => uint) bonds;
+	mapping(address => uint) shares;
+	address[] public Users;
 
+	function transferAmount(address _to, float _amount) public payable{
+		require(_amount <= balances[msg.sender]);
+		balances[msg.sender]-=_amount;
+		balances[_to]+=_amount;
 	}
+
+	function getBalance(address _owner) public view returns (float){
+		return balances[_owner];
+	}
+
+	
+	function mint(float _amount){
+		require(msg.sender == CentralBank);
+		balances[CentralBank]+=_amount;
+		uint totalShares = shares[CentralBank];
+		for (int i=0; i<Users.length;i++)
+		{
+			if (shares[i] > 0){
+				float new_coins = (shares[i] / float(totalShares)) * _amount;
+			}
+		}
+	}
+	
 	function burn(address _from,uint _amount) payable{
-		tokens[CentralBank]-=_amount;
-		tokens[_from]-=_amount;
+		balances[CentralBank]-=_amount;
+		balances[_from]-=_amount;
 	}
 }
