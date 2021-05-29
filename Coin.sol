b40dc6841630288a656bcc56cf2e99191cbbd6b9
tomerafek7 <58209550+tomerafek7@users.noreply.github.com>
2021-05-29 20:57:16 +0300

    Update Coin.sol
    compiling version (float --> uint + more minor changes)

--- Coin.sol
+++ Coin.sol
@@ -1,54 +1,55 @@
 pragma solidity ^0.5.0;
 
 contract Coin{
-	address CentralBank = 0xaaa;  //TODO: Fill Central Bank address here
-	mapping(address => float) balances;
+	address centralBankAddress = 0x0000000000000000000000000000000000000000;  //TODO: Fill Central Bank address here
+
+	mapping(address => uint) balances;
 	mapping(address => uint) bonds;
 	mapping(address => uint) shares;
-	address[] public Users;
+	address[] public users;
 
-	function transferAmount(address _to, float _amount) public payable{
+	function transferAmount(address _to, uint _amount) public payable{
 		require(_amount <= balances[msg.sender]);
 		balances[msg.sender]-=_amount;
 		balances[_to]+=_amount;
 	}
 
-	function getBalance(address _user) public view returns (float){
+	function getBalance(address _user) public view returns (uint){
 		return balances[_user];
 	}
 
 	
-	function InflateBalance(float _amount){
-		require(msg.sender == CentralBank);
-		balances[CentralBank]+=_amount;
-		uint totalShares = shares[CentralBank];
+	function InflateBalance(uint _amount) public{
+		require(msg.sender == centralBankAddress);
+		balances[centralBankAddress]+=_amount;
+		uint totalShares = shares[centralBankAddress];
 		// devide added amount to all users holding shares respectivley
-		for (int i=0; i<Users.length;i++)
+		for (uint i=0; i<users.length; i++)
 		{
-			if (shares[i] > 0){
-				float new_coins = (shares[i] / float(totalShares)) * _amount;
-				balances[i] += new_coins;
+			if (shares[users[i]] > 0){
+				uint new_coins = (shares[users[i]] / totalShares) * _amount;
+				balances[users[i]] += new_coins;
 			}
 		}
 	}
 	
-	function produceShare(address _to, uint _amount) payable{
-		require(msg.sender == CentralBank);
-		shares[CentralBank]+=_amount;
+	function produceShare(address _to, uint _amount) public payable{
+		require(msg.sender == centralBankAddress);
+		shares[centralBankAddress]+=_amount;
 		shares[_to]+=_amount;
 	}
 	
-	function sellBond(uint _amount) payable{
+	function sellBond(uint _amount) public payable{
 		require(bonds[msg.sender] >= _amount);
 		bonds[msg.sender]-=_amount;
 		
 		// User gets _amount of real USD $
 	}
 	
-	function buyBond(uint _amount) payable{
+	function buyBond(uint _amount) public payable{
 		require(balances[msg.sender] >= _amount);
 		bonds[msg.sender]+=_amount;
 		balances[msg.sender]-=_amount;
-		balances[CentralBank]-=_amount;
+		balances[centralBankAddress]-=_amount;
 	}
 }
