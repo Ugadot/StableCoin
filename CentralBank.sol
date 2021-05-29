fd4fe7216f9d23b6198a13a97fb0fce46dcaeed9
tomerafek7 <58209550+tomerafek7@users.noreply.github.com>
2021-05-29 20:56:20 +0300

    Update CentralBank.sol
    compiling version (float --> uint + more minor changes)

--- CentralBank.sol
+++ CentralBank.sol
@@ -1,12 +1,13 @@
 pragma solidity ^0.5.0;
+import "./Coin.sol";
 
 contract CentralBank{
 	//address Oracle = 0xaaa;  //TODO: Fill Oracle address here
 	uint mod = 256;
-	address CentralBank = 0xaaa;  //TODO: Fill Central Bank address here
+	address centralBankAddress = 0x0000000000000000000000000000000000000000;  //TODO: Fill Central Bank address here
 
 	//Coin to 2 ration if > 1 need to infalte coin base, if < 1 need users to buy bons and lower the amount of total coins
-	float C2D_ratio;
+	uint C2D_ratio;
   
 	function updateC2D(Coin _coin_contract) public {
 		// C2D_ration = Oracle.getRatio();
@@ -18,16 +19,16 @@ contract CentralBank{
 				))) % mod;
 		// Make ratio between 0.5 and 1.5
 
-		C2D_ratio = (randomNum + mod / 2) / float(mod / 2);
-		float current_amount = _coin_contract.getBalance(CentralBank);
+		C2D_ratio = (randomNum + mod / 2) / uint(mod / 2);
+		uint current_amount = _coin_contract.getBalance(centralBankAddress);
 		if (C2D_ratio > 1)
 		{
-			float _amount_to_inflate = current_amount * C2D_ratio - current_amount;
+			uint _amount_to_inflate = current_amount * C2D_ratio - current_amount;
 			_coin_contract.InflateBalance(_amount_to_inflate);
 		}
 	}
 	
-	function getC2D() public view returns (float){
+	function getC2D() public view returns (uint){
 		return C2D_ratio;
 	}
-};
+}
