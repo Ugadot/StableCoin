d9bd7b7a53450c009fc5f1c49d955ddec44fc2a1
tomerafek7 <58209550+tomerafek7@users.noreply.github.com>
2021-06-01 17:55:44 +0300

    Update CentralBank.sol

--- CentralBank.sol
+++ CentralBank.sol
@@ -3,27 +3,34 @@ import "./Coin.sol";
 
 contract CentralBank{
 	//address Oracle = 0xaaa;  //TODO: Fill Oracle address here
-	uint mod = 256;
-	address centralBankAddress = 0x0000000000000000000000000000000000000000;  //TODO: Fill Central Bank address here
+	uint mod = 100;
+	address centralBankAddress = address(this);
 
 	//Coin to 2 ration if > 1 need to infalte coin base, if < 1 need users to buy bons and lower the amount of total coins
 	uint C2D_ratio;
+	Coin _coin_contract;
+    event Log(address indexed sender, string message);
+    
+    function setCoin(address _coin_address) public {
+         _coin_contract = Coin(_coin_address);  
+    }
   
-	function updateC2D(Coin _coin_contract) public {
+	function updateC2D() public {
 		// C2D_ration = Oracle.getRatio();
-		// currently use random between [0.5, 1.5]
-		uint randomNum = uint(keccak256(abi.encodePacked(
+		// currently use random between [50, 150]
+		uint randomNum = (uint(keccak256(abi.encodePacked(
 				  now, 
 				  block.difficulty, 
 				  msg.sender
-				))) % mod;
-		// Make ratio between 0.5 and 1.5
-
-		C2D_ratio = (randomNum + mod / 2) / uint(mod / 2);
+				))) % mod);
+				
+		// Make ratio between mod/2 and mod*3/2
+		C2D_ratio = randomNum + mod/2;
 		uint current_amount = _coin_contract.getBalance(centralBankAddress);
-		if (C2D_ratio > 1)
+		if (C2D_ratio > mod)
 		{
-			uint _amount_to_inflate = current_amount * C2D_ratio - current_amount;
+            emit Log(msg.sender, "C2D_ratio > mod");
+			uint _amount_to_inflate = (current_amount * C2D_ratio) / mod - current_amount;
 			_coin_contract.InflateBalance(_amount_to_inflate);
 		}
 	}
