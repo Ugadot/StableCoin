8b2988d8175489d81e5e736168e70630612e0aad
tomerafek7 <58209550+tomerafek7@users.noreply.github.com>
2021-06-01 17:55:20 +0300

    Update Coin.sol

--- Coin.sol
+++ Coin.sol
@@ -1,16 +1,32 @@
 pragma solidity ^0.5.0;
 
 contract Coin{
-	address centralBankAddress = 0x0000000000000000000000000000000000000000;  //TODO: Fill Central Bank address here
+	address centralBankAddress = 0xd9145CCE52D386f254917e481eB44e9943F39138;
 
 	mapping(address => uint) balances;
 	mapping(address => uint) bonds;
 	mapping(address => uint) shares;
 	address[] public users;
+	uint startingAmount = 100;
+	uint constMultiplier = 1000;
 
+	function register() public {
+	    bool exists = false;
+		for (uint i=0; i<users.length; i++){
+		    if (users[i] == msg.sender){
+		        exists=true;
+		    }
+		}
+	    if (!exists){
+	        users.push(msg.sender);
+	        balances[msg.sender] += startingAmount;
+	        balances[centralBankAddress] += startingAmount;
+	    }
+	}
+	
 	function transferAmount(address _to, uint _amount) public payable{
-		require(_amount <= balances[msg.sender]);
-		balances[msg.sender]-=_amount;
+	    require(_amount <= balances[msg.sender]);
+	    balances[msg.sender]-=_amount;
 		balances[_to]+=_amount;
 	}
 
@@ -18,23 +34,25 @@ contract Coin{
 		return balances[_user];
 	}
 
+    function getShareBalance(address _user) public view returns (uint){
+		return shares[_user];
+	}
 	
 	function InflateBalance(uint _amount) public{
-		require(msg.sender == centralBankAddress);
+		//require(msg.sender == centralBankAddress);
 		balances[centralBankAddress]+=_amount;
 		uint totalShares = shares[centralBankAddress];
 		// devide added amount to all users holding shares respectivley
-		for (uint i=0; i<users.length; i++)
-		{
+		for (uint i=0; i<users.length; i++){
 			if (shares[users[i]] > 0){
-				uint new_coins = (shares[users[i]] / totalShares) * _amount;
-				balances[users[i]] += new_coins;
+				uint new_coins = (constMultiplier * shares[users[i]] / totalShares) * _amount;
+				balances[users[i]] += new_coins/constMultiplier;
 			}
 		}
 	}
 	
 	function produceShare(address _to, uint _amount) public payable{
-		require(msg.sender == centralBankAddress);
+		//require(msg.sender == centralBankAddress);
 		shares[centralBankAddress]+=_amount;
 		shares[_to]+=_amount;
 	}
