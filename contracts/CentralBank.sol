049fe1d6d5b08f886c323a4c2b3a6b78ab015ffd
root <root@DESKTOP-BVU6KLT.localdomain>
2021-06-05 19:48:39 +0300

    created orcale contract (not integrated yet)

--- contracts/CentralBank.sol
+++ contracts/CentralBank.sol
@@ -1,22 +1,30 @@
 pragma solidity ^0.5.0;
 import "./Coin.sol";
+import "./Oracle.sol";
 
 contract CentralBank{
-	//address Oracle = 0xaaa;  //TODO: Fill Oracle address here
 	uint mod = 100;
 	address centralBankAddress = address(this);
-
-	//Coin to 2 ration if > 1 need to infalte coin base, if < 1 need users to buy bons and lower the amount of total coins
-	uint C2D_ratio;
+	// Coin
 	Coin _coin_contract;
+	// Oracle
+	Oracle _oracle_contract;
+	//Coin to dollar ratio. (if > 1 need to infalte coin base, if < 1 need users to buy bons and lower the amount of total coins)
+	uint C2D_ratio;
+
     event Log(address indexed sender, string message);
     
     function setCoin(address _coin_address) public {
          _coin_contract = Coin(_coin_address);
     }
   
+    function setOracle(address _oracle_address) public {
+         _oracle_contract = Oracle(_oracle_address);
+    }
+	
 	function updateC2D() public {
-		// C2D_ration = Oracle.getRatio();
+		uint current_amount = _coin_contract.getBalance(centralBankAddress);
+		// C2D_ratio = _oracle_contract.getC2DRatio(current_amount);
 		// currently use random between [50, 150]
 		uint randomNum = (uint(keccak256(abi.encodePacked(
 				  now, 
@@ -26,7 +34,6 @@ contract CentralBank{
 				
 		// Make ratio between mod/2 and mod*3/2
 		C2D_ratio = randomNum + mod/2;
-		uint current_amount = _coin_contract.getBalance(centralBankAddress);
 		if (C2D_ratio > mod)
 		{
             emit Log(msg.sender, "C2D_ratio > mod");
@@ -41,5 +48,9 @@ contract CentralBank{
 	
 	function getCoinAddress() public view returns (address){
 		return address(_coin_contract);
+	}
+	
+	function getOracleAddress() public view returns (address){
+		return address(_oracle_contract);
 	}
 }
--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -1,1 +1,12 @@
+pragma solidity ^0.5.0;
 
+contract Oracle{
+
+	uint mul = 100; // multiply by this factor because we must work with integers.
+	uint dollars = 1000;
+    
+    function getC2DRatio(uint centralBankAmount) public returns (uint) {
+         return ((mul * centralBankAmount) / dollars); // a ratio of 100 means coin = dollar.
+    }
+
+}
